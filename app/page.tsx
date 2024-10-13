2e0045810b5e3329f73eb30a08e62b2d1c475cd9
Nishchay Bhatt <nishchay.bhat@gmail.com>
2024-10-13 20:46:08 +0530

    feat:Added Blogs

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,23 @@
 // import { Main, Section, Container } from "@/components/craft";
-
+"use client"
 import { Container, Layout, Main, Section } from "@/components/craft";
 import Clients from "@/components/home-page/clients";
 import Footer from "@/components/home-page/Footer";
 import Hero from "@/components/home-page/landing";
 import Statement from "@/components/home-page/statement";
+import { DefaultSeo } from "next-seo";
+import { useRouter } from "next/router";
 
 
 export default function Home() {
+  // const router = useRouter();
+  const canonicalUrl = `https://www.watersindia.com/`
   return (
     <>
-      
+    <DefaultSeo
+      canonical={canonicalUrl}
+    />
+
       <Layout>
         <Main className="dark text-foreground bg-background">
           <Section>
--- components/home-page/landing.tsx
+++ components/home-page/landing.tsx
@@ -14,11 +14,16 @@ const Hero = () => {
       <Container className="not-prose">
         <div className="flex w-full justify-between">
           <div className="">
-            <h1 className="text-3xl font-normal md:text-6xl">
-              <Link className="transition-all text-green-200 hover:opacity-70" href="/">
-                Waters India
-              </Link>
-            </h1>
+            <Link className="flex-row transition-all text-green-200 hover:opacity-70" href="/">
+              <div className="flex-row ">
+                <h1 className="text-3xl text-orange-600 font-normal md:text-6xl">
+                  {'Waters'}
+                </h1>
+                <h1 className="text-3xl text-white font-normal md:text-6xl">
+                  {'India'}</h1>
+              </div>
+
+            </Link>
 
             <div className="flex-row">
               <h2 className="w-82 text-lg text-green-200 font-light leading-6">
